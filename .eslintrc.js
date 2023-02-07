614d89395f78e1893198811a6465c0936138441e
nomiad <nomiadcode@gmail.com>
2023-02-07 17:44:25 +0100

    added default files for configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
     '@vue/eslint-config-standard-with-typescript',
     '@vue/eslint-config-standard-with-typescript/allow-tsx-in-vue'
   ],
+  parserOptions: {
+    project: './tsconfig.json',
+    extraFileExtensions: ['vue']
+  },
   rules: {
     'comma-dangle': ['error', 'only-multiline'],
     '@typescript-eslint/comma-dangle': ['error', 'only-multiline']
